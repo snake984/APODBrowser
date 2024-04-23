0f669f48f67ab5ba594114693047ed6f2e482c16
Tarek Smirani <tarek.smirani@protonmail.com>
2024-04-22 23:20:41 -0400

    refactor(ListComponents): Remove unused components

--- app/src/main/java/com/pandora/apodbrowser/ui/ListComponents.kt
+++ app/src/main/java/com/pandora/apodbrowser/ui/ListComponents.kt
@@ -2,25 +2,20 @@ package com.pandora.apodbrowser.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -33,7 +28,6 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.pandora.apodbrowser.R
 import com.pandora.apodbrowser.ui.model.PicOfTheDayItem
-import com.pandora.domain.model.PicOfTheDay
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
 import kotlinx.coroutines.flow.Flow
@@ -62,31 +56,6 @@ fun FullWidthPictureItem(
 }
 
 @Composable
-fun RandomPicsElement(
-    modifier: Modifier = Modifier,
-    picture: PicOfTheDay,
-) {
-    Column(
-        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        GlideImage(
-            modifier = Modifier
-                .size(88.dp)
-                .clip(CircleShape),
-            imageModel = { picture.url },
-            imageOptions = ImageOptions(
-                contentScale = ContentScale.Crop
-            ),
-        )
-        Text(
-            text = picture.date,
-            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
-            style = MaterialTheme.typography.bodyMedium,
-        )
-    }
-}
-
-@Composable
 fun LatestCollectionCard(
     modifier: Modifier = Modifier,
     picture: PicOfTheDayItem,
@@ -106,22 +75,6 @@ fun LatestCollectionCard(
                 contentScale = ContentScale.Crop
             )
         )
-    }
-}
-
-@Composable
-fun RandomPicsRow(
-    modifier: Modifier = Modifier,
-    data: List<PicOfTheDay>,
-) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp),
-        modifier = modifier
-    ) {
-        items(data) { item ->
-            RandomPicsElement(picture = item, modifier = modifier)
-        }
     }
 }
 
